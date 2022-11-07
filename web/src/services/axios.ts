4455f6a242acec1d0962f796c7b7099dab64ac9c
Maykon Eduardo Pereira de Sousa <maeduardo@uolinc.com>
2022-11-07 15:52:33 -0300

    feat: Create documentation

--- web/src/services/axios.ts
+++ web/src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const request = axios.create({
-  baseURL: 'https://nlwrankingserver-production.up.railway.app/',
+  baseURL: process.env.API_URL,
 });
 
 export const requestSkylab = axios.create();
